57cdfe24edcfcaa72563cbafcbb1dfc6cbcf32ee
Ashani Rasangika Dewmini <it21016820@my.sliit.lk>
2025-06-24 22:26:45 +0530

    Create card section and home

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,9 +1,13 @@
 import ImgCarousel from '@/components/ImgCarousel/ImgCarousel.jsx';
+import OverviewCards from '@/components/OverviewCards/OverviewCards.jsx';
+import WelcomeSection from '@/components/WelcomeSection/WelcomeSection.jsx';
 
 export default function Home() {
   return (
     <main>
-      <ImgCarousel />
+      {/* <ImgCarousel /> */}
+       <WelcomeSection />
+      <OverviewCards />
     </main>
   );
 }
